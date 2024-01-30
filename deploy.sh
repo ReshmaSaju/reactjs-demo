d3e8d2e46723a94678cd3efcf494520cb295817f
ReshmaSaju <139907678+ReshmaSaju@users.noreply.github.com>
2024-01-30 16:46:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@
 ./build.sh
 
 #docker login
-docker login --username reshma1215 --password dckr_pat_b1dJvrjmyy7YEzgsCyUmNDPh3-k
+docker login --username $DOCKERHUB_USERNAME --password dckr_pat_b1dJvrjmyy7YEzgsCyUmNDPh3-k
 
 #push  
 if [ "$GIT_BRANCH" == "origin/dev" ]; then
-  docker tag mydockerimage:alpine reshma1215/dev
-  docker push reshma1215/dev
+  docker tag mydockerimage:alpine $DOCKERHUB_USERNAME/dev
+  docker push $DOCKERHUB_USERNAME/dev
 elif [ "$GIT_BRANCH" == "origin/master" ]; then
-  docker tag mydockerimage:alpine reshma1215/prod
-  docker push reshma1215/prod
+  docker tag mydockerimage:alpine $DOCKERHUB_USERNAME/prod
+  docker push $DOCKERHUB_USERNAME/prod
 else
   echo "FAILED"
 fi
